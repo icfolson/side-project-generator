630b706b71a489979b3e29e4db58ffb62e7f0744
Nelson, Alexander <alexander.nelson@icfolson.com>
2017-06-28 17:33:43 -0500

    add files

---
+++ src/main.js
@@ -0,0 +1,68 @@
+//var ideaList = ['hedgehogs', 'pitbulls', 'salad', 'landmines', 'fairies', 'sailboats', 'mark wahlberg'];
+var ideaList = ['love/hate relationships', 'weird outfits', 'inventions', 'disappointment', 'school loans', '90s television shows', 'all-you-can-eat buffet', 'extreme minimalism', 'a massive ego', 'goats', 'Lil\' Wayne in his prime', 'Past Blue Ribbon', 'mouthwash', 'generic brand paper plates', 'friends with benefits', 'friends without benefits', 'anxiety', 'tiny lollipops', 'minor league sports teams', 'adults that use their childhood blankets', 'naughty robots', 'the pictures that come in picture frames', 'NSFW', 'SFW', 'people who use LOL', 'pillow fights', 'camaraderie', 'poor cooking skills', 'grandparents', 'parents', 'siblings', 'family', 'Facebook official', 'melted cheese', 'organ donation', 'the Cold War', 'the Miracle on Ice', 'Russia', 'not having a bed', 'dangerous knives', 'child actors', 'chivalry', 'grills', 'braces', 'mini horses', 'Canadian currency', 'food samples at the grocery store', 'child geniuses', 'jazz', 'classic rock', 'brand spokespeople', 'taboo drugs', 'yogurt', 'handwritten letters', 'passive-aggressive behavior', 'Donald Trump', 'rap mixtape', 'ancient weaponry', 'summer jams', 'pool floaties', 'Instagram', 'Twitter', 'Rome back in the day', 'America before Columbus', 'pop or soda?', 'lumberjacks', 'hand-to-hand combat', 'gymnastics', 'home runs', 'Facebook', 'Mille Dresselhaus', 'lying', 'vinyl', 'sleeping', 'happy meals', 'nerf guns', 'magic tricks', 'real magic', 'slippery slopes', 'Shia Labeouf', 'Egyptian pharaohs', 'heaven', 'haikus', 'sobriety', 'accounting', 'Major League Baseball', 'promiscuity', 'screenwriting', 'Craigslist', 'the dictionary', 'boycotting technology', 'art thieves', 'illegal graffiti', 'the 1%', 'emojis', 'DILFs', 'hiccups', 'awkward encounters', 'adult onesies', 'stand-up comedy', 'hipsters', 'participation awards', 'trading cards', 'Amazon Alexa', 'playful banter', 'bullying', 'tattooed models', 'high-waisted clothing', 'golf carts', 'public speaking', 'cooking recipes', 'fidget spinners', 'music videos', 'LinkedIn', 'Snapchat', 'stuffed animals', 'intimidation', 'being really nice', 'regrettable tattoos', 'Europe', 'South America', 'England', 'motorcycle gangs', 'auto-play video', 'pre-roll video', 'bad lip reading', 'retiring early', 'children\'s book', 'poetry',  'solving a problem', 'Tumblr', 'YouTube', 'Most Interesting Man in the World', 'opera', 'basketball', 'rodents', 'pens', 'computers', 'hangers', 'sunglasses', 'cars', 'car wash', 'motorcycles', 'bikes', 'flip phones', 'calling your mother', 'notebooks', 'animation', 'shoes', 'streetwear', 'action figures', 'dogs', 'cats', 'hamsters', 'birds', 'kites', 'alcohol', 'environmentalism', 'farms', 'the outdoors', 'video games', 'garbage', 'boats', 'soda', 'makeup', 'sponges', 'The White House', 'roller blades', 'soccer', 'football', 'spiders', 'endangered animals', 'global warming', 'drought', 'famine', 'war', 'partisanship', 'helicopters', 'nuclear power', 'fuel efficiency', 'water bottles', 'landmines', 'deep-sea exploration', 'colonizing Mars', 'space travel', 'sci-fi', 'fantasy', 'dragons', 'The Simpsons', 'cooking', 'Epic Meal Time', 'hot dog eating contest', 'celebrity chefs', 'aliens', 'sasquatch', 'the jungle', 'time travel', 'interior design', 'architecture', 'donuts', 'carnivals', 'glow in the dark', 'deconstructed', 'minimalism', 'clean', 'dirty', 'tacos', 'ice cream truck', 'improv', 'student debt', 'Star Wars', 'Star Trek', 'Klingons', 'Jedi', 'Sith', 'GIFs', 'graffiti', 'foxes', 'NASA', 'NSA', 'Army', 'Navy', 'Air Force', 'Coast Guard', 'Marines', 'submarines', 'Ellis Island', 'Coney Island', 'Manhattan', 'Brooklyn', 'bartender', 'surfing', 'scuba', 'fish', 'hats', 'industrial design', 'explosions', 'origami', 'ramen', 'sushi', 'cross fit', 'yoga', 'meditation', 'comic books', 'super heroes', 'gym teachers', 'locker rooms', 'puppets', 'geography', 'advertising', 'juggling', 'shoe laces', 'fishing', 'hiking', 'slow jams', 'speed walking', 'headphones', 'Harry Potter', 'Lord of the Rings', 'Books', 'Robots', 'Battle Bots', 'artificial intelligence', 'holograms', 'board games', 'tattoos', 'vintage', 'traffic', 'smog', 'ocean', 'MEMEs', 'bees', 'litter', 'whales', 'dolphins', 'sea otters', 'celebrities', 'paparazzi', 'vending machines', 'working out', 'worm holes', 'airplanes', 'crocodiles', 'greasy spoon', 'Netflix', 'Hulu', 'pizza', 'ultimate Frisbee', 'jean jacket', 'outdated internet trends', 'salads', 'Pokemon', 'polymath', 'racecar bed', 'spices', 'snakes', 'sneks', 'coloring book', 'Dr. Who', 'Sherlock Holmes', 'McDonalds', 'BK Lounge', 'Sonic', 'Taco Bell', 'travel', 'offensive', 'lasers', 'yard sales', 'political process', 'drama', 'melodramatic', 'gingers', 'Grover Cleveland', 'Dick Nixon', 'Bill Clinton', 'George H.W. Bush', 'body parts in jars', 'shrunken heads', 'voodoo', 'zombies', 'judges', 'mad scientist', 'D.I.Y. ', 'Mike Tyson', 'Scooby Doo', 'dancing', 'PokemonGo', 'shapeshifting', 'drunk', 'Roombas', 'roasts', 'gambling', 'freestyles', 'Super Mario', 'Zelda', 'Michael Caine 1969', 'Michael Caine 2012', 'Home Depot', '5-gallon bucket', 'Super Saiyen', 'doomsday', 'jazz flute', 'chicken nuggs', 'snapchat filters', '1980s WWF', 'Broadway play', 'mermaids', 'food fight', 'propaganda posters', 'John Cena', 'beer pong', 'tarot cards', 'the news', 'John Stewart', 'news anchors', 'stickers', 'Quentin Tarantino', 'Cockney accent', 'Michael Bay', '80s video games', 'watches', 'keys', 'vacation', 'periodic table', 'Family Guy', 'patches', 'perfume', 'soap', 'rock music', 'wine', 'beer', 'The Cloud', 'computer', 'first person shooter', 'weather', 'gas station', 'general store', 'wind chimes', 'The Rock', 'Rocky', 'darts', 'lawn darts', 'corn hole', 'drinking games', 'batteries', 'storm chaser', 'storm cellar', 'bunker', 'playing cards', '90s R&B', '2000s rap', 'phone cases', 'Quiddich', 'fitness', 'spooky mansions', 'graveyard', 'international space station', 'late-night TV', 'Playboy Mansion', 'desert', 'dessert', 'Halloween', 'Wi-Fi', 'cable TV', 'blackboard', 'auto-tune', 'career services', 'yo-yo', 'Razor scooter', 'The Dab', 'credit cards', 'rewards cards', 'public transportation', 'train line', 'fireplace', 'banking', 'used cars', 'raincoats', 'umbrellas', 'yard work', 'tree trimming', 'professional sports', 'tickets', 'tea', 'coffee', 'drive thru', 'logos', 'Legos', 'Ikea', 'safes', 'weapons', 'the Olympics', 'supplements', 'autopilot', 'Swedish design', 'disco', 'big band', 'healthcare', 'Adult Swim', 'sommelier', 'retro', 'retro-futuristic', 'futuristic', 'ancient Egypt', 'champagne', 'gardening', 'farmers market', 'cleaning supplies', 'maids', 'social media', 'car wrap', 'jewelry', 'award show', 'high fashion', 'calendars', 'cup holders', 'USPS', 'UPS', 'fortune cookies', 'parks', 'grilling', 'Will Smith', 'DC comics', 'Marvel', 'missing free throws', 'minor league mascots', 'getting stuck in the mud', 'a cold beverage', 'wired mouse', 'zoo', 'aquarium', 'historical documentary', 'camping', 'vaping', 'noodling', 'not understanding the language', 'sewer alligators', 'vampire hunter', 'Mother of Dragons', 'bookkeeping', 'rocket-propelled grenade', 'Great Wall of China', 'citrus', 'taking the stairs', 'missing puzzle piece', '3D glasses', 'Jurassic Park', 'body-snatching aliens', 'color blindness', 'scooter', 'fidget spinner repair', 'a bowl of Skittles with one M&M', 'non-authentic tacos', 'Hawaiian pizza', 'bail bonds', 'tow truck', 'dry cleaners', 'corn off the cob', 'lawn furniture', 'yard sale', 'volcanos', 'evil lair', 'Bat Cave', 'exploring a new world', 'avalanche recovery team', 'Disney princesses', 'hunting for sport', 'jelly bean identifier', 'Hogwarts test prep', 'composting', 'wind farm', 'distilling alcohol', 'weed farm', 'hugging strangers', 'floppy disks', 'VHS tapes', 'Sony Walkman', 'slow pitch softball', 'Dominos Pasta Bread Bowl', 'Candy Man', 'Beetlejuice', 'Willy Wonka', 'skinny', 'fat', 'fast', 'slow', 'weak', 'strong', 'helpful zombies', 'furries', 'Japanese body pillow', 'humans', 'archers', 'cannon', 'military war ships', 'Michael Jordan', 'LeBron James', 'shark teeth', 'lifeguards', 'Winnebago', 'outdoor plumbing', 'pirates', 'piñatas', 'new Oreo filling', 'too-small t-shirts', 'Romphim', 'manscaping', 'man cave', 'ballroom dancing', 'hot actors', 'equestrian', 'floppy hats', 'rompers', 'yoga', 'low-quality leggings', 'feeling like Beyoncé', 'avocado addiction', '#ootd', 'Lululemon', 'well-fed artists', 'ride sharing', 'bike sharing', 'too-hot coffee', 'socks with sandals', 'cookbook', 'wet socks', 'taxidermied animals', 'bug zappers', 'street signs', 'warning labels', 'dating site', 'beekeeping', 'avocado toast', 'Street Sharks', 'arcades', 'the devil', 'train-top chase', 'sleeping in a tauntaun', 'Kendrick Lamar', 'Aubrey Drake Graham', 'natural selection', 'dollar slices', 'the Pizza Rat', 'personal pan pizza', 'funeral home', 'manicure', 'pedicure', 'sundress', 'shopping', 'Rosé', 'The Bachelor', 'The Bachelorette', 'Ryan Gosling', 'The Notebook', 'Leonardo DiCaprio', 'maternity leave', 'babies', 'Wall Street', 'Sriracha', 'birthdays', 'MLK Day', 'paper airplanes', 'sunscreen', 'petition', 'New York Times', 'I like turtles', 'Russell Westbrook', 'Silicon Valley', 'dunk contest', 'Neil deGrasse Tyson', 'road trip', 'six pack', 'What would you do for a Klondike Bar?', 'conference call', 'meeting with HR', 'scarfs during summer', 'birds flying into airplanes', 'Tootsie Roll Pop', 'McRib', 'Megaphone', 'Bluetooth', 'wrong name', 'Starbucks cup', 'fake plants', 'first class', 'the Titanic', 'gentrification', 'punk rock', 'sad songs', 'Kanye West', 'the Kardashians', 'Kylie Jenner', 'Game of Thrones', 'fake news', 'Snap Spectacles'
+];
+var output = [0, 0];
+var isLeftLocked = document.getElementById("isLeftLocked");
+var isRightLocked = document.getElementById("isRightLocked");
+//var leftSelect = document.getElementById("leftSelect");
+
+function newOutput(){
+	if(isLeftLocked.checked === true && isRightLocked.checked === true){
+		alert("homie, you all locked out..");
+		return null;
+	}
+	else if(isLeftLocked.checked === true || isRightLocked.checked === true){
+		if(isLeftLocked.checked === true){
+			output[1] = getRandom();
+			while(output[1] === output[0]){
+				output[1] = getRandom();
+			}
+		}
+		if(isRightLocked.checked === true){
+			output[0] = getRandom();
+			while(output[1] === output[0]){
+				output[0] = getRandom();
+			}
+		}
+	}
+	else{
+		output[0] = getRandom();
+		output[1] = getRandom();
+		while(output[1] === output[0]){
+			output[1] = getRandom();
+		}
+	}
+}
+
+function myFunction(){
+		newOutput();
+		var leftDisplay = ideaList[output[0]];
+		var rightDisplay = ideaList[output[1]];
+		//for wiki page
+		// document.getElementById("leftOutput").href = "https://www.wikipedia.org/wiki/"+leftDisplay;
+		// document.getElementById("rightOutput").href = "https://www.wikipedia.org/wiki/"+rightDisplay;
+		//for google image page
+		document.getElementById("leftOutput").href = "http://images.google.com/search?tbm=isch&q="+textEdit(leftDisplay);
+		document.getElementById("rightOutput").href = "http://images.google.com/search?tbm=isch&q="+textEdit(rightDisplay);
+
+		document.getElementById("leftOutput").innerHTML = leftDisplay;
+		document.getElementById("rightOutput").innerHTML = rightDisplay;	
+}
+
+function getRandom(){
+	//var x = Math.floor(Math.random() * ideaList.length);
+	//x = Math.floor(Math.random() * ideaList.length);
+	// alert("success");
+	return Math.floor(Math.random() * ideaList.length);
+}
+function sendGoogle(idea){
+
+}
+function textEdit(stuff){
+	//http://images.google.com/search?tbm=isch&q=
+	return stuff.replace(/ /g, "_");
+}
+
+function searchLeftBy() {
+
+}
